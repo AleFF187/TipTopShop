65485ecfcce8d39e92c765d209c630772734a537
AleFF187 <1871847@gmail.com>
2021-04-23 18:02:27 +0200

    сделал кнопку и закончил бургер

--- js/main.js
+++ js/main.js
@@ -19,3 +19,28 @@ const swiper = new Swiper('.swiper-container', {
     //     el: '.swiper-scrollbar',
     // },
 });
+
+menuBtn = document.querySelector('.header__menu_button');
+menuList = document.querySelector('.header__menu_list');
+menuBtnItems = document.querySelectorAll('.button__item');
+menuBtnActive = false;
+
+console.log(menuBtnItems);
+menuBtn.addEventListener('click', function() {
+    // console.log(menuBtn);
+    if (menuBtnActive) {
+        menuList.style.display = '';
+        menuBtnActive = false;
+        menuBtnItems[0].classList.remove('button__item_1');
+        menuBtnItems[1].classList.remove('button__item_2');
+        menuBtnItems[2].classList.remove('button__item_3');
+
+    } else {
+        menuList.style.display = 'flex';
+        menuBtnActive = true;
+        menuBtnItems[0].classList.add('button__item_1');
+        menuBtnItems[1].classList.add('button__item_2');
+        menuBtnItems[2].classList.add('button__item_3');
+    }
+
+})
